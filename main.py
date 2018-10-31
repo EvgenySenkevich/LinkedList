58faf330d727e63b4dbd2e7590257f0638386365
Evgeny Senkevich <miks.qeury@yandex.ru>
2018-10-31 18:55:48 +0500

    add method len and test for him

--- main.py
+++ main.py
@@ -38,6 +38,14 @@ class LinkedList:
             past_node = node
             node = node.next
 
+    def len(self):
+        node = self.head
+        i = 0
+        while node is not None:
+            node = node.next
+            i += 1
+        return i
+
     def print_all_nodes(self):
         node = self.head
         while node != None:
@@ -51,8 +59,6 @@ if __name__ == "__main__":
     l_list.add_in_tail(Node(2))
     l_list.add_in_tail(Node(8))
     l_list.add_in_tail(Node(9))
+    l_list.add_in_tail(Node(9))
     l_list.print_all_nodes()
-
-    print()
-    l_list.delete(4)
-    l_list.print_all_nodes()
+    print("len = {}".format(l_list.len()))
--- test_module.py
+++ test_module.py
@@ -3,15 +3,15 @@ import main
 
 
 class LinkTest(unittest.TestCase):
-    a_list = main.LinkedList()
-    a_list.add_in_tail(main.Node(1))
-    a_list.add_in_tail(main.Node(2))
-    a_list.add_in_tail(main.Node(3))
-    a_list.add_in_tail(main.Node(4))
-    a_list.add_in_tail(main.Node(5))
 
     def test_add_in_tail(self):
-        node = self.a_list.head
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        a_list.add_in_tail(main.Node(4))
+        a_list.add_in_tail(main.Node(5))
+        node = a_list.head
         i = 1
         while node is not None:
             self.assertEqual(node.value, i)
@@ -19,16 +19,38 @@ class LinkTest(unittest.TestCase):
             i += 1
 
     def test_find(self):
-        self.assertEqual(self.a_list.find(2).value, 2)
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        a_list.add_in_tail(main.Node(4))
+        a_list.add_in_tail(main.Node(5))
+        self.assertEqual(a_list.find(2).value, 2)
 
     def test_delete(self):
-        self.a_list.delete(1)
-        node = self.a_list.head
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        a_list.add_in_tail(main.Node(4))
+        a_list.add_in_tail(main.Node(5))
+        a_list.delete(1)
+        node = a_list.head
         i = 2
         while node is not None:
+
             self.assertEqual(node.value, i)
             node = node.next
             i += 1
+
+    def test_len(self):
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        a_list.add_in_tail(main.Node(4))
+        a_list.add_in_tail(main.Node(5))
+        self.assertEqual(a_list.len(), 5)
 
 
 if __name__ == "__main__":
