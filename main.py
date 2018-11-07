8f369f9facb687a71a5a0ec31ce30b3abbed4355
Evgeny Senkevich <miks.qeury@yandex.ru>
2018-11-07 20:08:31 +0500

    add all method's and test's

--- main.py
+++ main.py
@@ -20,6 +20,52 @@ class LinkedList:
             item.prev = self.tail
         self.tail = item
 
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+
+    def delete(self, val):
+        node = self.head
+        if node is not None:
+            if node.value == val:
+                self.head = node.next
+        while node is not None:
+            if node.value == val:
+                node = node.prev
+                node.next = node.next.next
+                break
+            node = node.next
+
+    def add(self, node, val):
+        main_node = self.head
+        while main_node is not None:
+            if main_node.value == val:
+                node.next = main_node.next
+                main_node.next = node
+                node.prev = main_node
+                break
+            main_node = main_node.next
+
+    def add_first(self, node):
+        self.head, self.head.next = node, self.head
+        self.head.next.prev = self.head
+
+    def print_list(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
 
 if __name__ == "__main__":
-    pass
+    a_list = LinkedList()
+    a_list.add_in_tail(Node(1))
+    a_list.add_in_tail(Node(2))
+    a_list.add_in_tail(Node(3))
+    a_list.print_list()
+    a_list.add_first(Node(4))
+    print()
+    a_list.print_list()
--- test_module.py
+++ test_module.py
@@ -3,7 +3,68 @@ import main
 
 
 class LinkTest(unittest.TestCase):
-    pass
+
+    def __check_value(self, a_list, val):
+        node = a_list.head
+        while node is not None:
+            if node.value == val:
+                return True
+            node = node.next
+        return False
+
+    def test_add_in_tail(self):
+        a_list = main.LinkedList()
+        self.assertIsNone(a_list.head)
+        self.assertIsNone(a_list.tail)
+        a_list.add_in_tail(main.Node(1))
+        self.assertEqual(a_list.head.value, 1)
+        a_list.add_in_tail(main.Node(2))
+        self.assertEqual(a_list.head.next.value, 2)
+        self.assertEqual(a_list.tail.prev.value, 1)
+
+    def test_find(self):
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        self.assertIsNone(a_list.find(3))
+        a_list.add_in_tail(main.Node(3))
+        self.assertEqual(a_list.find(3).value, 3)
+
+    def test_delete(self):
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        self.assertTrue(self.__check_value(a_list, 2))
+        a_list.delete(2)
+        self.assertFalse(self.__check_value(a_list, 2))
+
+    def test_add(self):
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        self.assertFalse(self.__check_value(a_list, 4))
+        a_list.add(main.Node(4), 2)
+        self.assertTrue(self.__check_value(a_list, 4))
+        node = a_list.head
+        while node is not None:
+            if node.value == 4:
+                self.assertEqual(node.prev.value, 2)
+                self.assertEqual(node.next.value, 3)
+            node = node.next
+
+    def test_add_first(self):
+        a_list = main.LinkedList()
+        a_list.add_in_tail(main.Node(1))
+        a_list.add_in_tail(main.Node(2))
+        a_list.add_in_tail(main.Node(3))
+        self.assertEqual(a_list.head.value, 1)
+        a_list.add_first(main.Node(4))
+        self.assertEqual(a_list.head.value, 4)
+        self.assertEqual(a_list.head.next.value, 1)
+        node = a_list.head.next
+        self.assertEqual(node.prev.value, 4)
 
 
 if __name__ == "__main__":
